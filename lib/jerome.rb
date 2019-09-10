4c60ef62b05d88f6dcbd7307e59a811479f46864
Brendan Thomas <bt331609@gmail.com>
2019-09-09 21:39:19 -0400

    a simple translation app

--- lib/jerome.rb
+++ lib/jerome.rb
@@ -3,6 +3,39 @@
 require 'jerome/version'
 
 module Jerome
-  class Error < StandardError; end
-  # Your code goes here...
+  class NoTranslationError < StandardError; end
+
+  class << self
+    def dictionary
+      [
+        { left: 'dog', right: 'cane' },
+        { left: 'book', right: 'libro' },
+        { left: 'street', right: 'strada' },
+        { left: 'car', right: 'auto' },
+        { left: 'doctor', right: 'dottore' }
+      ]
+    end
+
+    def translate!(left)
+      translation = dictionary.find do |pairs|
+        left == pairs[:left]
+      end
+
+      raise NoTranslationError unless translation
+
+      translation[:right]
+    end
+
+    alias t! translate!
+
+    def reverse!(right)
+      translation = dictionary.find do |pairs|
+        right == pairs[:right]
+      end
+
+      raise NoTranslationError unless translation
+
+      translation[:left]
+    end
+  end
 end
--- spec/jerome_spec.rb
+++ spec/jerome_spec.rb
@@ -1,11 +1,47 @@
 # frozen_string_literal: true
 
 RSpec.describe Jerome do
-  it 'has a version number' do
-    expect(Jerome::VERSION).not_to be nil
+  before do
+    allow(Jerome).to receive(:dictionary).and_return dictionary
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:dictionary) do
+    [
+      { left: 'street', right: 'strada' }
+    ]
+  end
+
+  describe '.translate!' do
+    it 'returns the translated word' do
+      expect(Jerome.translate!('street')).to eq 'strada'
+    end
+
+    context 'when no translation can be found' do
+      it 'raises an error' do
+        expect { Jerome.translate! 'gobbledegook' }.to raise_error Jerome::NoTranslationError
+      end
+    end
+  end
+
+  describe '.t!' do
+    it 'is an alias of .translate!' do
+      expect(Jerome.method(:t!) == Jerome.method(:translate!))
+    end
+  end
+
+  describe '.reverse!' do
+    it 'returns the untranslated word' do
+      expect(Jerome.reverse!('strada')).to eq 'street'
+    end
+
+    context 'when no translation can be found' do
+      it 'raises an error' do
+        expect { Jerome.reverse! 'gobbledegook' }.to raise_error Jerome::NoTranslationError
+      end
+    end
+  end
+
+  it 'has a version number' do
+    expect(Jerome::VERSION).not_to be nil
   end
 end
